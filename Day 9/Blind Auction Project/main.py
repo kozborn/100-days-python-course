323e36511f8cc25fe550063f120c17412c6d6880
Piotr Kozubek <piotrkozubek@gmail.com>
2025-01-04 17:57:51 +0100

    Day - 9

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -3,4 +3,32 @@
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+from art import logo
+print(logo)
+bids = {}
+are_there_new_bidder = True
+while are_there_new_bidder:
+    name = input("What is your name?")
+    bid = float(input("What is your bid?"))
+    bids[name] = bid
+    is_there_anyone_else = input("Do you have anyone else? that would like to bid? (yes/no)").lower()
+    if is_there_anyone_else != "yes":
+        are_there_new_bidder = False
+    else:
+        print("\n" * 100)
+
+max_bid = 0
+max_bidder = None
+for name, bid in bids.items():
+    if bid > max_bid:
+        max_bidder = name
+        max_bid = bid
+
+print(f"Max bidder: {max_bidder} with bid {max_bid}")
+
+
+
+
+
+
 
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,27 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+}
+
+print(programming_dictionary["Bug"])
+
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+
+for student in student_scores:
+    score = student_scores[student]
+    if score <= 70:
+        student_scores[student] = "Fail"
+    elif 71 < score <= 80:
+        student_scores[student] = "Acceptable"
+    elif 81 < score <= 90:
+        student_scores[student] = "Exceeds Expectations"
+    elif 91 < score <= 100:
+        student_scores[student] = "Outstanding"
+print(student_scores)
