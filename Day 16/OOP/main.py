e16440d8ac9919b58d3b37f81fc3ecf0096c3da2
Piotr Kozubek <piotrkozubek@gmail.com>
2025-01-10 21:41:11 +0100

    Day - 16

---
+++ Day 16/OOP/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+timmy = Turtle()
+
+import prettytable
+
+table = prettytable.PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", 'Charmander'])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = 'l'
+
+
+print(table)
