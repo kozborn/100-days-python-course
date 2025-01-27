d2ee19df7556a9181b6971370e87284a489de6cc
Piotr Kozubek <piotrkozubek@gmail.com>
2025-01-27 22:17:01 +0100

    Day 35

---
+++ Day 35 - API Auth/main.py
@@ -0,0 +1,40 @@
+import requests
+import pywhatkit
+
+API_KEY = "8b4b7b321bf06c80e8529a5a721890d2"
+
+ENDPOINT_1 = f"https://api.openweathermap.org/data/2.5/weather?lat=49.95&lon=19.86&appid={API_KEY}"
+ENDPOINT_2 = "https://api.openweathermap.org/data/2.5/forecast"
+
+params = {
+    "q": "Krakow,PL",
+    "appid": API_KEY,
+    'units': 'metric',
+    'cnt': 4
+
+}
+
+
+response = requests.get(ENDPOINT_2, params=params)
+response.raise_for_status()
+
+weather_data = response.json()
+
+hour_data_list = [w['weather'] for w in weather_data["list"]]
+
+should_take_umbrella = False
+
+for hour_data in hour_data_list:
+    for weather in hour_data:
+        if int(weather['id']) < 700:
+            should_take_umbrella = True
+            break
+
+if should_take_umbrella:
+    print("You should take an umbrella")
+else:
+    print("You don't need an umbrella")
+
+
+
+
